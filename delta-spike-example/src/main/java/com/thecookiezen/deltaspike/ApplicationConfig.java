34d8fbb739d688aaa945ce0edf8c96e1d621f10b
Korneliusz Rabczak <nikom1337@gmail.com>
2016-10-15 16:27:25 +0200

    Initial commit

---
+++ delta-spike-example/src/main/java/com/thecookiezen/deltaspike/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.thecookiezen.deltaspike;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class ApplicationConfig extends Application {
+}
---
+++ delta-spike-example/src/main/java/com/thecookiezen/deltaspike/BooksResource.java
@@ -0,0 +1,16 @@
+package com.thecookiezen.deltaspike;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("book")
+public class BooksResource {
+
+    @GET
+    @Path("{id}")
+    public String getTitleById(@PathParam("id") int id) {
+        return "dupa " + id;
+    }
+
+}
